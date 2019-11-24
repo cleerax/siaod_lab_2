05a8b488b46505b3fee7ca2a7816acc3b5069772
Andrey Minaev <minaevabrikos@gmail.com>
2019-11-24 15:21:52 +0300

    CircleList done

--- circle.py
+++ circle.py
@@ -9,7 +9,8 @@ class CircleList:
 
     def push(self, value):
         if self.head == None:
-            self.head = Node(value, self.head)
+            self.head = Node(value)
+            self.head.next = self.head
         else:
             current = self.head
             while current.next != self.head:
@@ -24,5 +25,18 @@ class CircleList:
         else:
             current = self.head'''
 
-    def Head(self):
-        return self.head
+    def action(self):
+        current = self.head
+        temp = current.value
+        while current.next != self.head:
+            current.next.value, temp = current.next.value - temp, current.next.value
+            current = current.next
+        current.next.value -= temp
+
+        s = ""
+        current = self.head
+        while current.next != self.head:
+            s += str(current.value) + ", "
+            current = current.next
+        s += str(current.value)
+        return s
--- main2.py
+++ main2.py
@@ -16,27 +16,32 @@ class Ui_MainWindow(object):
         self.tab = QtWidgets.QWidget()
         self.tab.setObjectName("tab")
         self.lineEdit = QtWidgets.QLineEdit(self.tab)
-        self.lineEdit.setGeometry(QtCore.QRect(10, 10, 41, 31))
+        self.lineEdit.setGeometry(QtCore.QRect(10, 10, 251, 31))
         self.lineEdit.setObjectName("lineEdit")
         self.pushButton = QtWidgets.QPushButton(self.tab)
-        self.pushButton.setGeometry(QtCore.QRect(60, 10, 141, 32))
+        self.pushButton.setGeometry(QtCore.QRect(270, 0, 141, 51))
         self.pushButton.setObjectName("pushButton")
         self.pushButton.clicked.connect(self.btn1Clicked)
         self.label = QtWidgets.QLabel(self.tab)
-        self.label.setGeometry(QtCore.QRect(40, 50, 121, 16))
+        self.label.setGeometry(QtCore.QRect(10, 50, 121, 16))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.tab)
-        self.label_2.setGeometry(QtCore.QRect(40, 230, 131, 16))
+        self.label_2.setGeometry(QtCore.QRect(10, 200, 131, 16))
         self.label_2.setObjectName("label_2")
         self.pushButton_2 = QtWidgets.QPushButton(self.tab)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 140, 191, 91))
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 150, 401, 51))
         self.pushButton_2.setObjectName("pushButton_2")
-        self.textBrowser = QtWidgets.QTextBrowser(self.tab)
-        self.textBrowser.setGeometry(QtCore.QRect(10, 70, 191, 71))
-        self.textBrowser.setObjectName("textBrowser")
-        self.textBrowser_2 = QtWidgets.QTextBrowser(self.tab)
-        self.textBrowser_2.setGeometry(QtCore.QRect(10, 250, 191, 71))
-        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.pushButton_2.clicked.connect(self.btn2Clicked)
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.tab)
+        self.plainTextEdit.setGeometry(QtCore.QRect(10, 70, 401, 71))
+        self.plainTextEdit.setReadOnly(True)
+        self.plainTextEdit.setObjectName("plainTextEdit")
+        self.plainTextEdit.viewport().setCursor(QtCore.Qt.ArrowCursor)
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.tab)
+        self.plainTextEdit_2.setGeometry(QtCore.QRect(10, 220, 401, 71))
+        self.plainTextEdit_2.setReadOnly(True)
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.plainTextEdit_2.viewport().setCursor(QtCore.Qt.ArrowCursor)
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
         self.tab_2.setObjectName("tab_2")
@@ -50,22 +55,41 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Лабораторная работа номер два))))))))))))))))))))))))00"))
-        self.pushButton.setText(_translate("MainWindow", "Добавить элемент"))
+        self.pushButton.setText(_translate("MainWindow", "Сформировать\nсписок"))
         self.label.setText(_translate("MainWindow", "Созданный список"))
         self.label_2.setText(_translate("MainWindow", "Полученный список"))
-        self.pushButton_2.setText(_translate("MainWindow", "Вычесть из каждого\n"
-"последующего элемента\n"
-"предыдущий"))
+        self.pushButton_2.setText(_translate("MainWindow", "Вычесть из каждого\nпоследующего элемента предыдущий"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Кольцевой односвязный список"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Бинарное дерево"))
 
+    crcl = None
+
     def btn1Clicked(self):
         try:
-            crcl = CircleList()
-            crcl.push(int(self.lineEdit.text().strip()))
-            
+            self.crcl = CircleList()
+            s = self.lineEdit.text()
+            if s == "":
+                raise Exception("ничего не введено")
+
+            arr = list(map(int, s.split()))
+            s = ""
+            for i in arr:
+                self.crcl.push(i)
+                s += str(i) + ", "
+            s = s[:-2]
+            self.plainTextEdit.setPlainText(s)
+        except ValueError:
+            self.plainTextEdit.setPlainText("Список должен состоять из натуриальных чисел, введенных через пробел")
         except Exception as e:
-            print(str(e))
+            self.plainTextEdit.setPlainText("Ошибка: " + str(e))
+
+    def btn2Clicked(self):
+        try:
+            if not self.crcl:
+                raise Exception("список не заполнен")
+            self.plainTextEdit_2.setPlainText(self.crcl.action())
+        except Exception as e:
+            self.plainTextEdit_2.setPlainText("Ошибка: " + str(e))
 
 
 if __name__ == "__main__":
